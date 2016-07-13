dbe35eceb25cf3fcbf8b111cb7635db8f40cd1b8
Chris <cenne1986@qq.com>
2016-07-13 10:17:12 +0800

    Signed-off-by: Chris <cenne1986@qq.com>

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Editor</title>
-  <link href="/css/bootstrap.css" rel="stylesheet">
-  <link href="/css/editor.css" rel="stylesheet">
+  <link href="[[asset('css/bootstrap.css')]]" rel="stylesheet">
+  <link href="[[asset('css/editor.css')]]" rel="stylesheet">
   <style>
     .loading{
       width:128px;
@@ -16,7 +16,7 @@
       top:50%;
       left:50%;
       transform:translate(-50%,-50%); 
-      background: url('/img/preloader.gif');
+      background: url('[[asset('img/preloader.gif')]]');
     }
   </style>
 </head>
@@ -28,11 +28,11 @@
   <script type="text/javascript">
     document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>')
   </script> 
-  <script src="/js/libs/vue.js"></script>
+  <script src="[[asset('js/libs/vue.js')]]"></script>
 @else
-  <script src="/js/libs/vue.min.js"></script>
+  <script src="[[asset('js/libs/vue.min.js')]]"></script>
 @endif
-  <script src="/js/libs/jquery-1.12.3.min.js"></script>
-  <script src="/js/editor.js"></script>
+  <script src="[[asset('js/libs/jquery-1.12.3.min.js')]]"></script>
+  <script src="[[asset('js/editor.js')]]"></script>
 </body>
 </html>
