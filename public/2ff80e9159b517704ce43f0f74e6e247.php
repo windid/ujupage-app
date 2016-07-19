fb63a60956a90a6f8d8d033f7b6bee75e2ef468c
Chris <cenne1986@qq.com>
2016-07-19 17:34:38 +0800

    Signed-off-by: Chris <cenne1986@qq.com>

--- public/2ff80e9159b517704ce43f0f74e6e247.php
+++ public/2ff80e9159b517704ce43f0f74e6e247.php
@@ -9,7 +9,7 @@
 // 	$lastcommit = $hook->push_data->commits[count($hook->push_data->commits) - 1];
 // 	if (strstr($lastcommit->message, "Release")) {	
 	 	echo shell_exec("cd /opt/openresty/nginx/html/app_ujupage_com/; git pull 2>&1;");
-	 	echo shell_exec("composer --version 2>&1;");
+	 	echo shell_exec("composer --version 2>&1;"); 
 // 	}
 // }
 ?>
