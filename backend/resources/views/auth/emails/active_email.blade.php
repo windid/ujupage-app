6fb048a2e18ede5f9e0be3cc886db1e332c10301
Wendell <wendell.xie@gmail.com>
2016-12-08 11:25:08 +0800

    Test active email

--- backend/resources/views/auth/emails/active_email.blade.php
+++ backend/resources/views/auth/emails/active_email.blade.php
@@ -1,1 +1,8 @@
-点击链接激活: {{ url('active/'.$token) }}
+@extends('auth.master')
+
+@section('content')
+  <p>正在为您的聚页账户验证邮箱地址，请点击下面的链接：</p>
+  <div style="border-radius: 4px; background-color: #3E606F; color:#fff; font-size:18px; padding: 6px">
+    <a href="{{ url('/api/auth/active/'.$token) }}">验证我的邮箱地址</a>
+  </div>
+@endsection
