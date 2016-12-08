f0e9ce0d85b1653b4c883f6e35c48d92df339b3c
Wendell <wendell.xie@gmail.com>
2016-12-08 11:39:05 +0800

    Test email

--- backend/resources/views/auth/emails/active_email.blade.php
+++ backend/resources/views/auth/emails/active_email.blade.php
@@ -1,8 +1,8 @@
-@extends('auth.master')
+@extends('email.master')
 
 @section('content')
   <p>正在为您的聚页账户验证邮箱地址，请点击下面的链接：</p>
-  <div style="border-radius: 4px; background-color: #3E606F; color:#fff;">
+  <div style="border-radius: 4px; background-color: #3E606F;">
     <a href="{{ url('/api/auth/active/'.$token) }}" style="color: #fff; font-size:18px; padding: 9px 21px; text-decoration: none;">
       验证我的邮箱地址
     </a>
