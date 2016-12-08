bf9a23f6fb5c099c97a6dadbea7794deb278d2e9
xujiongbo <xujiongbo@gmail.com>
2016-12-08 11:40:41 +0800

    dat integrity fix

--- frontend/src/components/stats/Overview.vue
+++ frontend/src/components/stats/Overview.vue
@@ -3,6 +3,7 @@ import StatsNav from './StatsNav'
 import moment from 'moment'
 import { mapGetters } from 'vuex'
 import Chart from './charts/Chart'
+import { find } from 'lodash'
 
 export default {
   components: {
@@ -57,6 +58,11 @@ export default {
     },
     chartOptions () {
       return {
+        axisX: {
+          labelInterpolationFnc: value => {
+            return moment(value, 'YY-MM-DD').format('MM-DD')
+          }
+        },
         axisY: {
           labelInterpolationFnc: value => {
             return this.currentTab === 'conversionRate' ? value + '%' : value
@@ -65,13 +71,13 @@ export default {
         }
       }
     },
-    axisLabels () {
+    dayLabels () {
       const startDate = moment(this.params.start_date)
       const endDate = moment(this.params.end_date)
       const labels = []
       for (var i = 0; i <= endDate.diff(startDate, 'days'); i++) {
         const currentDate = moment(startDate).add(i, 'days').format('YYYY-MM-DD')
-        labels.push(moment(currentDate, 'YYYY-MM-DD').format('MM-DD'))
+        labels.push(currentDate)
       }
       return labels
     },
@@ -81,16 +87,20 @@ export default {
         conversionRate: [],
         visitors: []
       }
+
       if (this.report.variations) {
         this.report.variations.forEach(variation => {
           const conversions = []
           const conversionRate = []
           const visitors = []
-          variation.dates.forEach(d => {
-            conversions.push(parseFloat(d.total_conversions))
-            conversionRate.push(parseFloat((Math.round(d.cv * 1000) / 10.0)))
-            visitors.push(parseFloat(d.total_visitors))
+
+          this.dayLabels.forEach((day) => {
+            const vd = find(variation.dates, { report_date: day })
+            conversions.push(vd ? parseFloat(vd.total_conversions) : 0)
+            conversionRate.push(parseFloat((Math.round(vd ? vd.cv * 1000 : 0) / 10.0)))
+            visitors.push(vd ? parseFloat(vd.total_visitors) : 0)
           })
+
           data.conversions.push({
             name: variation.name,
             data: conversions
@@ -109,7 +119,7 @@ export default {
     },
     stats () {
       return {
-        labels: this.axisLabels,
+        labels: this.dayLabels,
         series: this.chartData[this.currentTab]
       }
     }
