27eb6d748b2285e1ba5a06cc75071e3c16c1073c
thenlai <tl_huang@outlook.com>
2016-12-22 10:44:47 +0800

    update: add keyboard shortcut to remove Element

--- frontend/src/components/editor/ElementCommon.vue
+++ frontend/src/components/editor/ElementCommon.vue
@@ -130,7 +130,12 @@ export default {
       const code = event.which || event.keyCode
       // 37 left, 39 right
       // 38 up, 40 down
-      if (code >= 37 && code <= 40) {
+      if (code === 8 || code === 46) {
+        // 删除, `backspace` or `delete`
+        this.removeElement([this.elementId])
+        event.stopPropagation()
+        event.preventDefault()
+      } else if (code >= 37 && code <= 40) {
         event.stopPropagation()
         event.preventDefault()
         if (this.keyMoveTimer === null) {
