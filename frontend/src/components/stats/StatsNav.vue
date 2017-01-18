8215b780c8c353d70c0f9752c603c931a281a719
xujiongbo <xujiongbo@gmail.com>
2017-01-18 13:32:08 +0800

    remove moment dependency

--- frontend/src/components/stats/StatsNav.vue
+++ frontend/src/components/stats/StatsNav.vue
@@ -1,7 +1,7 @@
 <script>
 import DateRangePicker from '../ui/DateRangePicker'
 import Dropdown from '../ui/Dropdown'
-import moment from 'moment'
+import moment from '../../utils/date'
 
 export default {
   props: ['title'],
