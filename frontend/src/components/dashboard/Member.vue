4e9cebbf96e3cc99f77b8bd3feb42ee83a05ba20
Wendell <wendell.xie@gmail.com>
2017-04-01 11:00:27 +0800

    fix member list tooltip bug

--- frontend/src/components/dashboard/Member.vue
+++ frontend/src/components/dashboard/Member.vue
@@ -75,21 +75,21 @@
 </script>
 
 <template>
-  <tooltip data-toggle="dropdown" v-model="show" :manual="true" @click.native="toggle">
+  <tooltip data-toggle="dropdown" v-model="show" :manual="true" @click.native="toggle" placement="bottom">
     <div>
       {{memberName}}
-      <div ref="tooltip" slot="content" class="tooltip-content">
-        <p v-if="memberRole === 'invited'">
-          {{member.email}}<br>
-          已邀请<br>
-        </p>
-        <p v-else>
-          {{member.name}}<br>
-          {{member.email}}<br>
-          {{memberRole === 'admin' ? '项目管理员' : '项目成员'}}<br>
-        </p>
-        <div v-if="isAdmin && memberRole !== 'admin'" class="btn btn-xs btn-danger" @click="remove">移除</div>
-      </div>
+    </div>
+    <div ref="tooltip" slot="content" class="tooltip-content">
+      <p v-if="memberRole === 'invited'">
+        {{member.email}}<br>
+        已邀请<br>
+      </p>
+      <p v-else>
+        {{member.name}}<br>
+        {{member.email}}<br>
+        {{memberRole === 'admin' ? '项目管理员' : '项目成员'}}<br>
+      </p>
+      <div v-if="isAdmin && memberRole !== 'admin'" class="btn btn-xs btn-danger" @click="remove">移除</div>
     </div>
   </tooltip>
 </template>
